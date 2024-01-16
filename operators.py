c5db523ad5872c093fb6e0ea1a63282785f48cbb
balaramvarma <balaramvarma.marolix@gmail.com>
2024-01-16 17:39:58 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,37 @@
+#Arithemetic operators
+print(10+17)
+print(10-10)
+print(14/7)
+print(14//4)
+print(7*9)
+print(2**3)
+print(7%3)
+print("str"+"char")
+print("sstr"*10)
+
+
+#comparsion operators
+print(5>2)  
+print(5==2)
+print(5!=2)
+print(5>2)
+print(5<2)
+print(5>=5)
+print(8<=8)
+print("srt">"hh")
+print("ssff"=="srsff")
+print("python"!="code")
+print("Python"<"python")
+
+
+#logical operators
+print(True and True)
+print(True and False)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or False)
+
+print(not True)
+print(not False)
